42f0468999706e229512596583a97f12581b3cf8
jakebitz <jacobbitters@gmail.com>
2022-08-01 20:13:27 -0500

    angular sucks

--- lesson1/exercises/src/app/app.module.ts
+++ lesson1/exercises/src/app/app.module.ts
@@ -5,13 +5,21 @@ import { AppComponent } from './app.component';
 import { MovieListComponent } from './movie-list/movie-list.component';
 import { ChoresListComponent } from './chores-list/chores-list.component';
 import { FavPhotosComponent } from './fav-photos/fav-photos.component';
+import { HeaderComponent } from './header/header.component';
+import { CrewComponent } from './crew/crew.component';
+import { EquipmentComponent } from './equipment/equipment.component';
+import { ExperimentComponent } from './experiment/experiment.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MovieListComponent,
     ChoresListComponent,
-    FavPhotosComponent
+    FavPhotosComponent,
+    HeaderComponent,
+    CrewComponent,
+    EquipmentComponent,
+    ExperimentComponent
   ],
   imports: [
     BrowserModule
